2171a01d5e7639f1c188c5f5333d749764a99d44
Robson Cesar de Siqueira <sistemas.robson@gmail.com>
2024-12-14 12:02:42 -0300

    WIP IngredientsListViewController

--- RxSwift-Recipes/RxSwift-Recipes/ViewController/IngredientsListViewController.swift
+++ RxSwift-Recipes/RxSwift-Recipes/ViewController/IngredientsListViewController.swift
@@ -9,11 +9,68 @@ import UIKit
 
 class IngredientsListViewController: UIViewController {
 
+    private var tableView: UITableView = UITableView(frame: .zero, style: .insetGrouped)
+
+    private var dataSource: UITableViewDiffableDataSource<Int, String>!
+
+    private var ingredients = ["Tomato", "Onion", "Garlic", "Pepper", "Basil"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        title = "Ingredients"
+        
+        setupTableView()
+        setupDataSource()
+        applySnapshot()
     }
 
+    private func setupTableView() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 
+    private func setupDataSource() {
+        dataSource = UITableViewDiffableDataSource<Int, String>(tableView: tableView) { tableView, indexPath, ingredient in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "IngredientCell") ?? UITableViewCell(style: .default, reuseIdentifier: "IngredientCell")
+            cell.textLabel?.text = ingredient
+            return cell
+        }
+        dataSource.defaultRowAnimation = .fade
+        tableView.delegate = self
+    }
+
+    private func applySnapshot() {
+        var snapshot = NSDiffableDataSourceSnapshot<Int, String>()
+        snapshot.appendSections([0])
+        snapshot.appendItems(ingredients)
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
+    
+    private func deleteIngredient(at indexPath: IndexPath) {
+        guard let ingredient = dataSource.itemIdentifier(for: indexPath) else { return }
+        
+        ingredients.removeAll { $0 == ingredient }
+        applySnapshot()
+    }
 }
 
+extension IngredientsListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { [weak self] _, _, completion in
+            self?.deleteIngredient(at: indexPath)
+            completion(true)
+        }
+        
+        return UISwipeActionsConfiguration(actions: [deleteAction])
+    }
+}
